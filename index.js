80b31cb9c290b8932b2ce15749c6ecf102354e60
Taras Mankovski <tarasm@gmail.com>
2015-03-14 06:05:45 -0400

    Added pack from Bower

--- index.js
+++ index.js
@@ -4,54 +4,64 @@
 module.exports = {
   name: 'ember-list-view',
   treeForVendor: function() {
+    var trees = ['vendor'];
 
-    if(!this.isDevelopingAddon()) {
-      return;
+    var pack = new this.Funnel('bower_components', {
+      srcDir: '/pack',
+      files: ['index.js'],
+      destDir: '/',
+      getDestinationPath: function(relativePath) {
+        return 'pack.js'
+      }
+    });
+    trees.push(pack);
+
+    if (this.isDevelopingAddon()) {
+      var klassy = new this.Funnel('bower_components', {
+        srcDir: '/klassy/lib',
+        files: ['klassy.js'],
+        destDir: '/'
+      });
+
+      var emberTestHelpers = new this.Funnel('bower_components', {
+        srcDir: '/ember-test-helpers/lib',
+        include: [/.js$/],
+        destDir: '/'
+      });
+
+      var es6EmberTestHelpers = new this.transpileModules(this.mergeTrees([klassy, emberTestHelpers]), {
+        esperantoOptions: {
+          _evilES3SafeReExports: true,
+          strict: true
+        }
+      });
+
+      var emberTestHelpersAMD = this.concatFiles(es6EmberTestHelpers, {
+        inputFiles: ['**/*.js'],
+        outputFile: '/ember-test-helpers.amd.js'
+      });
+      trees.push(emberTestHelpersAMD);
+
+      var qunitModule = new this.Funnel('bower_components', {
+        srcDir: '/ember-qunit-source/lib/ember-qunit',
+        include: [/qunit-module.js$/],
+        destDir: '/'
+      });
+
+      var es6QunitModule = new this.transpileModules(this.mergeTrees([klassy, qunitModule]), {
+        esperantoOptions: {
+          _evilES3SafeReExports: true,
+          strict: true
+        }
+      });
+
+      var qunitModuleAMD = this.concatFiles(es6QunitModule, {
+        inputFiles: ['**/*.js'],
+        outputFile: '/ember-qunit-module.amd.js'
+      });
+      trees.push(qunitModuleAMD);
     }
 
-
-    var klassy = new this.Funnel('bower_components', {
-      srcDir: '/klassy/lib',
-      files: ['klassy.js'],
-      destDir: '/'
-    });
-
-    var emberTestHelpers = new this.Funnel('bower_components', {
-      srcDir: '/ember-test-helpers/lib',
-      include: [/.js$/],
-      destDir: '/'
-    });
-
-    var es6EmberTestHelpers = new this.transpileModules(this.mergeTrees([klassy, emberTestHelpers]), {
-      esperantoOptions: {
-        _evilES3SafeReExports: true,
-        strict: true
-      }
-    });
-
-    var emberTestHelpersAMD = this.concatFiles(es6EmberTestHelpers, {
-      inputFiles: ['**/*.js'],
-      outputFile: '/ember-test-helpers.amd.js'
-    });
-
-    var qunitModule = new this.Funnel('bower_components', {
-      srcDir: '/ember-qunit-source/lib/ember-qunit',
-      include: [/qunit-module.js$/],
-      destDir: '/'
-    });
-
-    var es6QunitModule = new this.transpileModules(this.mergeTrees([klassy, qunitModule]), {
-      esperantoOptions: {
-        _evilES3SafeReExports: true,
-        strict: true
-      }
-    });
-
-    var qunitModuleAMD = this.concatFiles(es6QunitModule, {
-      inputFiles: ['**/*.js'],
-      outputFile: '/ember-qunit-module.amd.js'
-    });
-
-    return this.mergeTrees(['vendor', emberTestHelpersAMD, qunitModuleAMD]);
+    return this.mergeTrees(trees);
   }
 };
